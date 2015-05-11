e72c814e68784fb743d36cc92d9ca2def5df08fd
apetri <apetri@phys.columbia.edu>
2015-05-11 10:55:06 -0400

    contiguous grouping

--- code/scaling.py
+++ code/scaling.py
@@ -5,7 +5,7 @@ def variance_scaling(ens,group_sizes,num_samples=100):
 	variance = []
 
 	for group_size in group_sizes:
-		ens_grouped = ens.group(group_size,"sparse",inplace=False).subset(range(num_samples))
+		ens_grouped = ens.group(group_size,"contiguous",inplace=False).subset(range(num_samples))
 		variance.append(ens_grouped.covariance().diagonal())
 
 	return np.array(variance)
