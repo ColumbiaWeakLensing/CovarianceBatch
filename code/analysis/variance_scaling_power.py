b05617bc97012c59d294d2f6efb987403f2dd46c
apetri <apetri@phys.columbia.edu>
2015-11-18 10:17:27 -0500

    garbage collect in variance_scaling_power

--- code/analysis/variance_scaling_power.py
+++ code/analysis/variance_scaling_power.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import division
+import gc
 
 from lenstools.statistics.database import Database
 from lenstools.statistics.ensemble import Series,Ensemble
@@ -27,16 +28,24 @@ def main():
 	#Large and small scales
 	scales = {
 	"large" : filter(lambda s:int(s.strip("l"))<=7,emulator[emulator.feature_names[0]].columns),
-	"small" : filter(lambda s:int(s.strip("l"))>7,emulator[emulator.feature_names[0]].columns), 
+	"small" : filter(lambda s:int(s.strip("l"))>7,emulator[emulator.feature_names[0]].columns),
+	"all" :  list(emulator[emulator.feature_names[0]].columns),
 	}
 
 	#Fiducial cosmology
 	fiducial_parameters = Series(np.array([0.26,-1.0,0.8]),index=["Om","w","sigma8"])
 
 	#Load in the feature Ensemble, and bootstrap the covariance using a different number of realizations
-	with Database("../../data/variance_scaling_expected.sqlite") as db:
+	with Database("../../data/variance_scaling_power_expected.sqlite") as db:
 	
 		for s in scales:
+
+			#Log
+			table_name = "variance_" + s
+			print("[+] Populating table {0}...".format(table_name))
+
+			#Approximate the emulator linearly around the fiducial model
+			fisher = emulator.approximate_linear(fiducial_parameters,derivative_precision=0.01).features({"power_spectrum":scales[s]})
 
 			#This is the reference covariance matrix
 			true_covariance_ensemble = Ensemble(np.load("../../Om0.260_Ol0.740_w-1.000_ns0.960_si0.800/512b240/Maps200/power_spectrum_s0.npy"),columns=emulator[emulator.feature_names[0]].columns)[scales[s]]
@@ -49,14 +58,14 @@ def main():
 				ensemble_nsim = Ensemble(np.load("../../Om0.260_Ol0.740_w-1.000_ns0.960_si0.800/512b240/Maps{0}/power_spectrum_s0.npy".format(n)),columns=emulator[emulator.feature_names[0]].columns)[scales[s]]
 				ensemble_nsim.add_name("power_spectrum")
 
-				#Approximate the emulator linearly around the fiducial model
-				fisher = emulator.approximate_linear(fiducial_parameters,derivative_precision=0.01).features({"power_spectrum":scales[s]})
-
 				#Bootstrap ensemble_sim and compute the parameter variance for each resample
 				for nr in nreal:
-					print("[+] Bootstraping scale={0}, nsim={1}, nreal={2} with {3} resamples".format(s,n,nr,resample))
+
+					gc.collect()
+
+					print("[+] Bootstraping scale={0}, Nb={1}, nsim={2}, nreal={3} with {4} resamples".format(s,ensemble_nsim.shape[1],n,nr,resample))
 					variance_ensemble = ensemble_nsim.bootstrap(algorithms.bootstrap_fisher,bootstrap_size=nr,resample=resample,assemble=lambda l:Ensemble.concat(l,ignore_index=True),fisher=fisher,true_covariance=true_covariance,extra_items={"nsim":n,"nreal":nr})
-					db.insert(Ensemble(variance_ensemble.mean()).T,table_name=s)
+					db.insert(Ensemble(variance_ensemble.mean()).T,table_name=table_name)
 
 
 if __name__=="__main__":
