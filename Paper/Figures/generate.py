b2a835b4c098b7bbe82cb6638e2951e7b68dfa2f
apetri <apetri@phys.columbia.edu>
2015-12-03 15:30:14 -0500

    finished implementing modifications with O(1/N^2) predictions

--- Paper/Figures/generate.py
+++ Paper/Figures/generate.py
@@ -43,34 +43,51 @@ markers = {
 colors = {
 "power_logb_large" : "black",
 "power_logb_small" : "black",
-"power_logb_all" : "black",
+"power_logb_all" : "red",
 "power_large" : "red",
 "power_small" : "red",
-"power_large+small" : "red",
-"power_all" : "red",
-"peaks_low" : "green",
-"peaks_intermediate" : "green",
-"peaks_high" : "green",
+"power_large+small" : "green",
+"power_all" : "blue",
+"peaks_low" : "red",
+"peaks_intermediate" : "red",
+"peaks_high" : "red",
 "peaks_low+intermediate" : "green",
 "peaks_intermediate+high" : "green",
-"peaks_all" : "green",
+"peaks_all" : "magenta",
 } 
 
 #Labels
 labels = {
-"power_logb_large" : "Power spectrum log binning",
+"power_logb_large" : r"$N_b=7\div 8$",
 "power_logb_small" : None,
 "power_logb_all" : None,
 "power_large" : None,
-"power_small" : None,
-"power_large+small" : None,
-"power_all" : "Power spectrum linear binning",
-"peaks_low" : "Peak counts",
+"power_small" : r"$N_b=15$",
+"power_large+small" : r"$N_b=30$",
+"power_all" : r"$N_b=39$",
+"peaks_low" : None,
 "peaks_intermediate" : None,
 "peaks_high" : None,
 "peaks_low+intermediate" : None,
 "peaks_intermediate+high" : None,
-"peaks_all" : None,
+"peaks_all" : r"$N_b=45$",
+} 
+
+#Plot order
+order = {
+"power_logb_large" : 1,
+"power_logb_small" : -1,
+"power_logb_all" : -1,
+"power_large" : -1,
+"power_small" : 2,
+"power_large+small" : 3,
+"power_all" : 4,
+"peaks_low" : -1,
+"peaks_intermediate" : -1,
+"peaks_high" : -1,
+"peaks_low+intermediate" : -1,
+"peaks_intermediate+high" : -1,
+"peaks_all" : 5,
 } 
 
 #Offsets
@@ -259,7 +276,7 @@ def scaling_ns(cmd_args,db_filename="variance_scaling_nb_expected.sqlite",featur
 	#Labels
 	ax.set_xlim(-10,210)
 	ax.set_xlabel(r"$N_s$",fontsize=fontsize)
-	ax.set_ylabel(r"$\sigma_0^2(N_s)/\sigma_{0,mean}^2$",fontsize=fontsize)
+	ax.set_ylabel(r"$\sigma_\infty^2(N_s)/\sigma_{\infty,mean}^2$",fontsize=fontsize)
 	ax.legend()
 
 	#Save
@@ -299,7 +316,7 @@ def effective_nb(cmd_args,db_filename="variance_scaling_nb_expected.sqlite",para
 ###########################################################################################################################################
 
 #Curving effect of the variance versus 1/Nr fir different Nb
-def curving_nb(cmd_args,db_filename="variance_scaling_nb_expected.sqlite",parameter="w",nsim=200,fontsize=22,figname="curving_nb"):
+def curving_nb(cmd_args,db_filename="variance_scaling_nb_expected.sqlite",parameter="w",nsim=200,xlim=(0,1./65),ylim=(1,2.5),nr_top=[1000,500,300,200,150,100,90,70],fontsize=22,figname="curving_nb"):
 
 	#Plot panel
 	fig,ax = plt.subplots() 
@@ -308,21 +325,48 @@ def curving_nb(cmd_args,db_filename="variance_scaling_nb_expected.sqlite",parame
 
 	#Load the database and fit for the effective dimensionality of each feature space
 	with Database("data/"+db_filename) as db:
+
+		features  = db.tables
+		features.sort(key=order.get)
 		
-		for f in db.tables:
+		for f in features:
 
 			#Read the table corresponding to each feature
 			v = db.read_table(f).query("nsim=={0}".format(nsim))
 			v["1/nreal"] = v.eval("1.0/nreal")
 			v = v.sort_values("1/nreal")
 
+			#Find the variance in the limit of large Nr
+			s0 = algorithms.fit_nbins(v,kind="linear",vfilter=lambda d:d.query("nreal>=500")).query("nsim=={0}".format(nsim))["s0"].mean()
+
+			#Nb,Np
+			Nb = v["bins"].mean()
+			Np = 3
+
 			#Plot the variance versus 1/nreal
-			ax.plot(v["1/nreal"],v["w"]/v["w"].iloc[0],color=colors[f],marker=markers[f],label=labels[f],linewidth=1+(5-1)*(v["bins"].mean()-7)/(50-7),markersize=10+(20-10)*(v["bins"].mean()-1)/(200-1))
+			ax.scatter(v["1/nreal"],v["w"]/s0,color=colors[f],marker=markers[f],label=labels[f],s=10+(100-10)*(Nb-1)/(200-1))
+
+			#Plot the theory predictions
+			x = 1./np.linspace(1000,65,100)
+			ax.plot(x,1+x*(Nb-Np),linestyle="--",color=colors[f])
+			ax.plot(x,1+(Nb-Np)*x+(Nb-Np)*(Nb-Np+2)*(x**2),linestyle="-",color=colors[f])
+
+
+	#Axis bounds
+	ax.set_xlim(*xlim)
+	ax.set_ylim(*ylim)
 
 	#Axis labels and legends
 	ax.set_xlabel(r"$1/N_r$",fontsize=fontsize)
-	ax.set_ylabel(r"$\langle\hat{\sigma}^2_w\rangle/\langle\hat{\sigma}^2_w\rangle(N_r=1000)$",fontsize=fontsize)
+	ax.set_ylabel(r"$\langle\hat{\sigma}^2_w\rangle/\sigma^2_{w,\infty}$",fontsize=fontsize)
 	ax.legend(loc="upper left")
+
+	#Mirror x axis to show Nr on top
+	ax1 = ax.twiny()
+	ax1.set_xlim(*xlim)
+	ax1.set_xticks([1./n for n in nr_top])
+	ax1.set_xticklabels([str(n) for n in nr_top])
+	ax1.set_xlabel(r"$N_r$",fontsize=fontsize)
 
 	#Save the figure
 	fig.savefig(".".join([figname,cmd_args.type]))
@@ -330,14 +374,72 @@ def curving_nb(cmd_args,db_filename="variance_scaling_nb_expected.sqlite",parame
 		
 ###########################################################################################################################################
 
+#Mean feature as a function of Nsim
+def means_nsim(cmd_args,fontsize=22,figname="means_ns"):
+
+	#Number of simulations
+	nsim = [1,2,5,10,20,30,40,50,60,70,80,90,100,150,200]
+
+	#Multipoles and peak thresholds
+	ell = np.load("features/ell.npy")
+	vpk = np.load("features/th_peaks.npy")
+
+	#Multipoles to select
+	ell_select = [0,8,-1]
+
+	#Peak thresholds to select
+	vpk_select = [10,20,30]
+
+	#Create the labels
+	labels = [r"$l={0}$".format(int(ell[n])) for n in ell_select] + [r"$\kappa_0={0:.2f}$".format(vpk[n]) for n in vpk_select]
+
+	#Load all the features
+	power_spectrum = np.empty((len(ell_select),len(nsim)))
+	for n,ns in enumerate(nsim):
+		ensemble_mean = np.load("features/Maps{0}/power_spectrum_s0.npy".format(ns)).mean(0)
+		power_spectrum[:,n] = ensemble_mean[ell_select]
+
+	power_spectrum_std = np.load("features/Maps200/power_spectrum_s0.npy").std(0)[ell_select]
+
+	peaks = np.empty((len(vpk_select),len(nsim)))
+	for n,ns in enumerate(nsim):
+		ensemble_mean = np.load("features/Maps{0}/peaks_s0.npy".format(ns)).mean(0)
+		peaks[:,n] = ensemble_mean[vpk_select]
+
+	peaks_std = np.load("features/Maps200/peaks_s0.npy").std(0)[vpk_select]
+
+	all_features = np.vstack((power_spectrum,peaks))
+	all_features_std = np.hstack((power_spectrum_std,peaks_std))
+
+	#Plot the ensemble means as a function of nsim
+	fig,ax = plt.subplots()
+	for n,f in enumerate(all_features):
+		ax.plot(nsim,(f-f[-1])/all_features_std[n],label=labels[n])
+
+	#Plot the 10% accuracy line for reference
+	ax.plot(np.linspace(0,210,3),np.ones(3)*0.1,linestyle="--",linewidth=2,color="black")
+
+	#Labels
+	ax.set_xlim(-10,210)
+	ax.set_ylim(-0.8,0.65)
+	ax.legend()
+	ax.set_xlabel(r"$N_s$",fontsize=fontsize)
+	ax.set_ylabel(r"$[d_i(N_s)-d_i(200)]/\sqrt{C_{ii}(200)}$",fontsize=fontsize)
+
+	#Save
+	fig.savefig(".".join([figname,cmd_args.type]))
+
+###########################################################################################################################################
+
 #Method dictionary
 method = dict()
 method["1"] = ps_pdf
-method["2"] = ps_variance
-method["3"] = scaling_nr
-method["4"] = scaling_ns
-method["5"] = curving_nb
-method["6"] = effective_nb
+method["2"] = means_nsim
+method["3"] = ps_variance
+method["4"] = curving_nb
+method["5"] = scaling_nr
+method["6"] = scaling_ns
+method["7"] = effective_nb
 
 #Main
 def main():
